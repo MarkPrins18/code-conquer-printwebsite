06a16d0cfb10939c766ea01ccdf2a141e8d8a1c5
David Vreugdenhil <d.vreugdenhil@safetyct.com>
2025-10-08 13:17:45 +0200

    highlight selected example

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,11 +3,11 @@ const contents = document.querySelectorAll(".content-item");
 
 items.forEach((item) => {
   item.addEventListener("click", () => {
-    // eerst alles verbergen
     contents.forEach((c) => (c.style.display = "none"));
-    // juiste content tonen
     const id = item.dataset.content;
     document.getElementById(id).style.display = "flex";
+    items.forEach((i) => i.classList.remove("selected"));
+    item.classList.add("selected");
   });
 });
 items[0].click();
