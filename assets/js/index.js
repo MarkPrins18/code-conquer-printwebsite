20b961f5330c2ca62ba999418527b3de1be40cca
David Vreugdenhil <d.vreugdenhil@safetyct.com>
2025-10-26 17:02:23 +0100

    changed order of scripts

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,3 @@
-const items = document.querySelectorAll(".select-example li");
-const contents = document.querySelectorAll(".content-item");
-
-items.forEach((item) => {
-  item.addEventListener("click", () => {
-    contents.forEach((c) => (c.style.display = "none"));
-    const id = item.dataset.content;
-    document.getElementById(id).style.display = "flex";
-    items.forEach((i) => i.classList.remove("selected"));
-    item.classList.add("selected");
-  });
-});
-items[0].click();
-
 const counters = document.querySelectorAll(".counter");
 
 counters.forEach((counter) => {
@@ -29,3 +15,17 @@ counters.forEach((counter) => {
     updateCounter();
   }, 1000);
 });
+
+const items = document.querySelectorAll(".select-example li");
+const contents = document.querySelectorAll(".content-item");
+
+items.forEach((item) => {
+  item.addEventListener("click", () => {
+    contents.forEach((c) => (c.style.display = "none"));
+    const id = item.dataset.content;
+    document.getElementById(id).style.display = "flex";
+    items.forEach((i) => i.classList.remove("selected"));
+    item.classList.add("selected");
+  });
+});
+items[0].click();
