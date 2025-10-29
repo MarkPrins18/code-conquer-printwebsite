383f5a614a3d6869f285b428dcc45d9fc62ab5af
David Vreugdenhil <d.vreugdenhil@safetyct.com>
2025-10-29 13:27:08 +0100

    Added beginning of admin page

---
+++ admin.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="nl">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="assets/css/global.css" />
+    <link rel="stylesheet" href="assets/css/header-footer.css" />
+    <link rel="stylesheet" href="assets/css/admin.css" />
+    <script src="assets/js/admin.js" defer></script>
+    <title>admin portaal</title>
+    <link rel="icon" type="image/x-icon" href="assets/images/favicon.ico" />
+  </head>
+  <body>
+    <!--This code should be put on any page. The pages needs to have extension .php. Html files can't run php code.-->
+    <?php include 'layout/header.html' ?>
+
+    <main></main>
+
+    <?php include 'layout/footer.html' ?>
+  </body>
+</html>
