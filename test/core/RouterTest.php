cf97ae529b95248cd77103844e84bd1f7df2cbb1
David Vreugdenhil <d.vreugdenhil@safetyct.com>
2026-06-15 21:44:11 +0200

    Added tests

--- test/core/RouterTest.php
+++ test/core/RouterTest.php
@@ -8,13 +8,17 @@ class TestController
 {
     public static bool $indexCalled = false;
     public static bool $showCalled = false;
+    public static bool $showOrderCalled = false;
     public static ?string $receivedId = null;
+    public static ?string $receivedOrderId = null;
 
     public static function reset(): void
     {
         self::$indexCalled = false;
         self::$showCalled = false;
+        self::$showOrderCalled = false;
         self::$receivedId = null;
+        self::$receivedOrderId = null;
     }
 
     public function index(): void
@@ -26,6 +30,13 @@ class TestController
     {
         self::$showCalled = true;
         self::$receivedId = $id;
+    }
+
+    public function showOrder(string $id, string $orderId): void
+    {
+        self::$showOrderCalled = true;
+        self::$receivedId = $id;
+        self::$receivedOrderId = $orderId;
     }
 }
 
@@ -154,10 +165,50 @@ class RouterTest extends TestCase
 
     public function testMultipleParametersArePassed(): void
     {
-        $router = new class extends Router {
-            public array $params = [];
-        };
+        $router = new Router();
 
-        $this->assertTrue(true);
+        $router->get('/users/{id}/orders/{orderId}', [TestController::class, 'showOrder']);
+
+        $router->dispatch('/users/42/orders/7', 'GET');
+
+        $this->assertTrue(TestController::$showOrderCalled);
+        $this->assertSame('42', TestController::$receivedId);
+        $this->assertSame('7', TestController::$receivedOrderId);
+    }
+
+    public function testPutRouteCallsControllerMethod(): void
+    {
+        $router = new Router();
+
+        $router->put('/users/{id}', [TestController::class, 'show']);
+
+        $router->dispatch('/users/99', 'PUT');
+
+        $this->assertTrue(TestController::$showCalled);
+        $this->assertSame('99', TestController::$receivedId);
+    }
+
+    public function testPatchRouteCallsControllerMethod(): void
+    {
+        $router = new Router();
+
+        $router->patch('/users/{id}', [TestController::class, 'show']);
+
+        $router->dispatch('/users/99', 'PATCH');
+
+        $this->assertTrue(TestController::$showCalled);
+        $this->assertSame('99', TestController::$receivedId);
+    }
+
+    public function testDeleteRouteCallsControllerMethod(): void
+    {
+        $router = new Router();
+
+        $router->delete('/users/{id}', [TestController::class, 'show']);
+
+        $router->dispatch('/users/99', 'DELETE');
+
+        $this->assertTrue(TestController::$showCalled);
+        $this->assertSame('99', TestController::$receivedId);
     }
 }
